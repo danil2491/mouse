d6cc94fd3404757c7699f7e0ae60919cda2615a0
danil2491 <146551406+danil2491@users.noreply.github.com>
2023-10-12 19:14:28 +0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+#https://github.com/danil2491/mouse.git
+from pygame import *
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set.caption("Мышеловка")
+background = transform.scale(image.load(''),(win_width, win_height))
+
+mixer.init()
+mixer.music.load('')
+mixer.music.play()
+ 
